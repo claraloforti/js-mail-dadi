e5059b3445cb90436b0563d9c5f4c4366e6c2dec
Clara Loforti <clara.loforti@gmail.com>
2025-12-04 13:12:44 +0100

    inizializzo es

---
+++ script.js
@@ -0,0 +1,11 @@
+/*ESERCIZIO MAIL
+Crea una lista di email di invitati ad una festa.
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo, utilizzando un ciclo for.
+*/
+
+/*ESERCIZIO GIOCO DEI DADI
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+*/
