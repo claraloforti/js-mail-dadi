7b4416e5a4c4cfd03f37e5b5c9c00ac979fd0bcd
Clara Loforti <clara.loforti@gmail.com>
2025-12-04 16:17:18 +0100

    formatto

--- js-mail/mail.js
+++ js-mail/mail.js
@@ -3,7 +3,7 @@
 // Creo un array con le email degli invitati alla festa
 const guestsEmail = ['clara@guest.com', 'andrea@guest.com', 'anna@guest.com', 'luca@guest.com']
 // Chiedo all'utente la sua email e verifico se è presente nella lista di chi può accedere alla festa
-const email = prompt("Scrivi la tua email");
+const email = prompt("Inserisci la tua email");
 let presente = false;
 let messaggio = ("Mi dispiace, la tua email non è presente nella lista degli invitati!");
 
