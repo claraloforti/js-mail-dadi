e19b3c46586b8c7ef298fe9f0161bf0251c0fb15
Clara Loforti <clara.loforti@gmail.com>
2025-12-04 17:47:24 +0100

    formatto

--- js-dadi/dadi.js
+++ js-dadi/dadi.js
@@ -3,15 +3,15 @@
 // Genero un numero random da 1 a 6 per l'User e uno per il PC
 const diceUser = Math.floor(Math.random() * 6) + 1;
 const dicePC = Math.floor(Math.random() * 6) + 1;
-const betUser = `Puntata User: ${diceUser}`;
-const betPC = `Puntata PC: ${dicePC}`;
+const betUser = `Il numero generato dal tuo dado è: ${diceUser}`;
+const betPC = `Il numero generato dal dado del Computer è: ${dicePC}`;
 let risultato;
 
 // Chi fa il punteggio più alto vince
 if (diceUser > dicePC) {
-    risultato = ('Hai vinto!');
+    risultato = ('Congratulazioni, hai vinto!');
 } else if (diceUser < dicePC) {
-    risultato = ('Hai perso!');
+    risultato = ('Mi dispiace, hai perso!');
 } else {
     risultato = ('Pareggio!');
 }
